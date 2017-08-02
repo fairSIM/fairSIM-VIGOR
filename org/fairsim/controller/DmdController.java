6d09be7b7b135cae320413c90c52e1c531500a82
Carina Wenzel <carina.wenzel@uni-bielefeld.de>
2017-08-02 15:42:34 +0200

    Improved error handling

--- org/fairsim/controller/DmdController.java
+++ org/fairsim/controller/DmdController.java
@@ -17,10 +17,16 @@ along with fairSIM.  If not, see <http://www.gnu.org/licenses/>
  */
 package org.fairsim.controller;
 
-import org.fairsim.utils.Conf;
+import java.io.BufferedReader;
 import org.fairsim.utils.Tool;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -28,61 +34,129 @@ import java.io.FileNotFoundException;
  */
 public class DmdController implements SlmController {
     
-    //ControllerServerGui gui;
+    ControllerServerGui gui;
     
-    private DmdController(/*ControllerServerGui serverGui*/) {
+    public DmdController(ControllerServerGui serverGui) {
         
-        //this.gui = serverGui;
+        this.gui = serverGui;
         
         // Loading hidapi-Library
         String wd = System.getProperty("user.dir")+"\\";
         String libName = "hidapi";
         System.load(wd+libName+".dll");
         
+        this.gui.showText("Dmd: loading "+ wd + libName +".dll");
+        
         // Loading DMD-API-Library
         wd = System.getProperty("user.dir")+"\\";
         libName = "dlp6500-java-api";
         System.load(wd+libName+".dll");
+
+        this.gui.showText("Dmd: loading "+ wd + libName + ".dll");
     }
     
-    public static String filelist(final Conf.Folder cfg) throws FileNotFoundException {{
-        String folder;// = "(not found)";
-    
-        try {
-            folder = Tool.getFile(cfg.getStr("DmdRoFolder").val()).getAbsolutePath();
-            System.out.println("Folder"+folder);
-        } catch (Conf.EntryNotFoundException ex) {
-            folder = System.getProperty("user.dir");
-            Tool.error("No folder was found. Folder was set to: " + folder, false);
+    public static String[] filelist() throws FileNotFoundException, IOException {
+        
+        String filename = Tool.getFile(System.getProperty("user.home") + "/NetBeansProjects/fairSIMproject/vigor-tmp").getAbsolutePath();
+        File f = new File("C:/Users/cwenzel/Documents/NetBeansProjects/fairSIMproject/vigor-tmp");
+        File[] fileArray = f.listFiles();
+       // ArrayList filelist = new ArrayList();
+        String[] stringArray = new String[fileArray.length];
+        if(fileArray != null){
+            for(int i=0;i<fileArray.length;i++){
+                if(fileArray[i].getName().contains("_batch")){
+                    if(checkfile(fileArray[i].getAbsoluteFile())){
+                    String fileNameWithOutExtension = fileArray[i].getName();
+                    int help;
+                    help = fileArray[i].getName().lastIndexOf('.');
+                    if (help != -1)
+                    fileNameWithOutExtension = fileNameWithOutExtension.substring(0, help);
+                    stringArray[i] = fileNameWithOutExtension;
+                   // filelist.add(fileNameWithOutExtension);        
+                }
+                else{System.out.println("file is not compartible");}
+                } 
+            }
+           
         }
-         File file = new File(folder);
-        if (file.exists()) {
-            return folder;
-        } else {
-            throw new FileNotFoundException("Folder does not exists");
-        }
-        }
+        
+        
+    return stringArray;
     }
     
-    String[] filelist = {"C:\\Users\\cwenzel\\Documents\\NetBeansProjects\\fairSIMproject\\vigor-tmp\\linespoints.txt","C:\\Users\\cwenzel\\Documents\\NetBeansProjects\\fairSIMproject\\vigor-tmp\\points.txt"};
+    public static boolean checkfile(File f) throws FileNotFoundException, IOException{
+        boolean boo = false;
+        FileReader fr = new FileReader(f.getAbsolutePath());
+        BufferedReader br = new BufferedReader(fr);
+        int count=1;
+        for(int i = 0; i<count;i++){
+        String line=br.readLine();
+        if(line != null){
+           if(line.contains("MBOX_DATA")){
+//               System.out.println("MBOX");
+               count++;
+               boo= true;
+           }
+           else if(line.contains("PAT_CONFIG")){
+//               System.out.println("PAT Config");
+               count++;
+               boo= true;
+           }
+           else if(line.contains("PATMEM_LOAD_INIT_MASTER")){
+//               System.out.println("PAT LOAD INIT MASTER");
+               count++;
+               boo= true;
+           }
+           else if(line.contains("PATMEM_LOAD_DATA_MASTER")){
+//               System.out.println("PAT LOAD DATA MASTER");
+               count++;
+               boo= true;
+           }
+           else{
+               boo= false;
+               break;
+           }
+        }
+        }
+        
+        return boo;
+    }
+    
+    public static void testsetRo(int ro) throws IOException{
+        String[] list;
+            list = filelist();
+            String file = "C:/Users/cwenzel/Documents/NetBeansProjects/fairSIMproject/vigor-tmp"+list[ro]+".txt";
+            System.out.println("It was chosen file "+file);
+        
+    }
 
-
-
+    // the board is activated, the mode is tested and perhaps changed, the file is uploaded
     @Override
     public String setRo(int ro) {
+        String[] list;
         try {
             activateBoard();
             if(isActive()){System.out.println("Board is activ");}
             if(getMode()==2||getMode()==0) setMode(3);
-            executeBatchFile(filelist[ro]);
-            System.out.println("It was chosen file "+filelist[ro]);
+            list = filelist();
+            String file = "C:\\Users\\cwenzel\\Documents\\NetBeansProjects\\fairSIMproject\\vigor-tmp\\"+list[ro]+".txt";
+            executeBatchFile(file);
+            
+            gui.showText("Selected running order '" + ro + "'");
+            
+            System.out.println("It was chosen file "+file);
             return "File was chosen";
         } catch (DmdException ex) {
             System.out.println("fail in setRo");
             return DmdException.catchedDmdException(ex); 
         }
+        catch(IOException ex){
+            Logger.getLogger(DmdController.class.getName()).log(Level.SEVERE, null, ex);
+            return "building up the file failed";
+        }
     }
-
+    
+    //the sequence is started and it is tested if its running
     @Override
     public String activateRo() {
         try {
@@ -92,59 +166,84 @@ public class DmdController implements SlmController {
                 }
             else{
                 System.out.println("Activated current sequence in mode "+getMode());
+                gui.showText("Activated running order" );
                 return "Current sequence got activated"; 
                 }
         } catch (DmdException ex) {
             return DmdException.catchedDmdException(ex);
         }
     }
-
+    
+    //the sequence is stopped and it is tested if its running
     @Override
     public String deactivateRo() {
         try {
             stopSequence();
-            System.out.println("Deactivated current sequence");
+            if(isSequenceRunning()){
+                return "Sequence coud not be deactivated.";
+            }
+            else{
+                gui.showText("Deactivated current running order");
+                System.out.println("Deactivated current sequence");
+            }
             return "Current sequence got deactivated";
         } catch (DmdException ex) {
             return DmdException.catchedDmdException(ex);
         }
     }
-
+    
+    //the list of sequences is outlined
     @Override
     public String getRoList() {
-        for (String filelist1 : filelist) {
-            System.out.println(filelist1);
+        String[] list;
+        try {
+            list = filelist();
+            for(int i = 0; i<list.length;i++){
+            gui.showText("ArrayList of sequences constructed");
+            System.out.println(list[i]);            
         }
-            return "Filelist has been output completely.";
+        } catch (IOException ex) {
+            Logger.getLogger(DmdController.class.getName()).log(Level.SEVERE, null, ex);//DmdException is not possible here !!
+        }
+        return "file list was created.";
+        //return Tool.encodeArray("Transfering rolist", list);
           }
-
+    
+    
     @Override
     public String getSlmInfo() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    //it happens a new start of the DMD
     @Override
     public String rebootSlm() {
         try {
-            resetBoard();
+            deactivateBoard();
+            disconnect();
+            connect();
             System.out.println("DMD is rebooting");
+            gui.showText("Dmd is rebooting");
             return "Reboot of the DMD. This may takes more than 2 seconds";
         } catch (DmdException ex) {
             return DmdException.catchedDmdException(ex);
         }
     }
 
+    //the connection to the DMD is builded
     @Override
     public String connectSlm() {
         try {
             connect();
             System.out.println("Connection to the DMD opened.");
+            gui.showText("Connection to the Dmd opened.");
             return "Connected to the DMD";
         } catch (DmdException ex) {
             return DmdException.catchedDmdException(ex);
         }
     }
-
+    
+    //the connection to the DMD is lost
     @Override
     public String disconnectSlm() {
         try {
@@ -152,6 +251,7 @@ public class DmdController implements SlmController {
             System.out.println("Board is inactiv");
             disconnect();
             System.out.println("Disconnection from the DMD.");
+            gui.showText("Deactivation of the board was successful.");
                 return "Disconnected from the DMD";
         } catch (DmdException ex) {
             return DmdException.catchedDmdException(ex);
@@ -268,34 +368,54 @@ public class DmdController implements SlmController {
     //Modus erst setzen, dann Board ativieren, dann Sequenz starten, letzteres klappt nicht
     //disconnect klappt
     
-//    public Boolean isDmd(SlmController slm){
-//        Boolean dmd = false;
-//        if (slm.connectSlm() =="Connected to the DMD"){
-//            dmd=true;
-//        }
-//        return dmd;
-//    }
     
-    public static void main(String[] args) throws InterruptedException, DmdException {
+    public int einlesen() {
+ String eingabe = JOptionPane.showInputDialog("Zahl eingeben!", "1");
+ try {
+ int zahl= Integer.parseInt(eingabe);
+ return zahl;
+ } catch (NumberFormatException e) {
+ System.out.println(eingabe + " ist keine Zahl");
+ return(-1);
+ }
+}
+    
+    public static void main(String[] args) throws InterruptedException, DmdException, IOException {
+        for(int i=0;i<filelist().length;i++){
+            System.out.println(filelist()[i]);
+        }
         
-        DmdController dmd = new DmdController();
-        //filelist("C:\\Users\\cwenzel\\Documents\\NetBeansProjects\\fairSIM\\vigor-omx-config-windows.xml");
+//        DmdController dmd = new DmdController();
+//
+//        dmd.connectSlm();
+//        dmd.getRoList();
+//        
+//        dmd.setRo(dmd.einlesen());
+//        dmd.activateRo();
+//        Thread.sleep(2000);
+//        dmd.deactivateRo();
+//        dmd.rebootSlm();
+//        Thread.sleep(5000);
+//        dmd.setRo(dmd.einlesen());
+//        dmd.activateRo();
+//        Thread.sleep(2000);
+//        dmd.disconnectSlm();
+     //   dmd.executeBatchFile("C:/Users/cwenzel/Documents/NetBeansProjects/fairSIMproject/vigor-tmp/Block_Load.batch.txt");
+     //   dmd.startSequence();
         
-        dmd.getRoList();
-        dmd.connectSlm();
-        System.out.println("setRo: "+dmd.setRo(0));
-        System.out.println("activateRo: "+dmd.activateRo());
-        Thread.sleep(2000);
-        System.out.println("deactivatRo: "+dmd.deactivateRo());
-        Thread.sleep(2000);
-        System.out.println("resetBoard: "+dmd.rebootSlm());
-        Thread.sleep(5000);
-        System.out.println("connecting "+dmd.connectSlm());
-        System.out.println("setRo: "+dmd.setRo(1));
-        System.out.println("activateRo: "+dmd.activateRo());
-        Thread.sleep(2000);
-        dmd.disconnectSlm();
-
+//        System.out.println("setRo: "+dmd.setRo(0));
+//        System.out.println("activateRo: "+dmd.activateRo());
+//        Thread.sleep(2000);
+//        System.out.println("deactivatRo: "+dmd.deactivateRo());
+//        Thread.sleep(2000);
+//        System.out.println("resetBoard: "+dmd.rebootSlm());
+//        Thread.sleep(5000);
+//        System.out.println("connecting "+dmd.connectSlm());
+//        System.out.println("setRo: "+dmd.setRo(1));
+//        System.out.println("activateRo: "+dmd.activateRo());
+//        Thread.sleep(2000);
+//        dmd.disconnectSlm();
+//
     }
     
 }
